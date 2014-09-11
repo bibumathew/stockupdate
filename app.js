84708456b53d8aef7ae701c325bba84c346b01b7
Bibu Mathew <bibumathew@gmail.com>
2014-09-11 18:12:09 -0500

    initial server push code

---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ * Created by bimathew on 9/3/14.
+ */
+var express = require('express'),
+    http = require('http'),
+    request = require('request'),
+    app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server),
+    LISTEN_PORT = 4500,
+    BASE_STOCK_URL = 'https://finance.google.com/finance/info?client=ig&q='
+STOCK_LIST = 'EBAY,GOOG,APPL,TSLA';
+
+app.use(express.static(__dirname + '/.'));
+server.listen(LISTEN_PORT)
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/views/index.html');
+});
+function pushStockData(socket) {
+    var requestUrl = BASE_STOCK_URL + STOCK_LIST;
+    request.get(requestUrl, function (error, response, body) {
+        if (error) {
+            return false;
+        }
+        socket.emit("loadStock", body);
+    });
+}
+io.sockets.on('connection', function (socket) {
+    pushStockData(socket);
+   /* var timer = setInterval(function () {
+        pushStockData(socket)
+    }, 3000);*/
+    socket.on('disconnect', function () {
+        //clearInterval(0);
+    });
+
+});
+
+module.exports = app;
--- bin/server.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by bimathew on 9/11/2014.
- */
