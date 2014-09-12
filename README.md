04badd33113312921f75530787f60962c8e4c826
Bibu <bibumathew@gmail.com>
2014-09-12 13:16:40 -0500

    Removed packages

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 stockupdate
 ===========
+
+Run the Follwing commands from stockupdate directory
+
+1 npm install
+2 npm start
+
--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     LISTEN_PORT = 4500,
     BASE_STOCK_URL = 'https://finance.google.com/finance/info?client=ig&q=',
-    STOCK_LIST = 'EBAY,GOOG,APPL,TSLA';
+    STOCK_LIST = 'EBAY,GOOG,AAPL,TSLA';
 
 app.use(express.static(__dirname + '/.'));
 server.listen(LISTEN_PORT)
@@ -17,6 +17,7 @@ server.listen(LISTEN_PORT)
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/views/index.html');
 });
+
 function pushStockData(socket) {
     var requestUrl = BASE_STOCK_URL + STOCK_LIST;
     request.get(requestUrl, function (error, response, body) {
